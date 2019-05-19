a0fcff8b0fd01d1212ee8fbdb70993700493eebb
Sebastian <ssosarippe@gmail.com>
2019-05-18 21:52:08 -0300

    Add airline data app

---
+++ airlines-data/README.md
@@ -0,0 +1,16 @@
+# Airline data
+This app exposes airline data via a REST API.
+
+GET localhost:8080/airlines/
+GET localhost:8080/airlines/:id
+
+[
+  {
+    id: "UA",
+    name: "United Air Lines Inc."
+  },
+  {
+    id: "AA",
+    name: "American Airlines Inc."
+  },
+}
---
+++ airlines-data/app/config/ioc/container.js
@@ -0,0 +1,28 @@
+const { asClass, createContainer, Lifetime } = require('awilix')
+const { scopePerRequest } = require('awilix-express')
+
+const wrapAsync = require('../middleware/wrapAsync')
+
+const container = createContainer()
+
+container.register({
+  wrapAsync: asClass(wrapAsync).scoped()
+})
+
+container.loadModules(
+  [
+    [`${__dirname}/../../**/*.service.js`, { register: asClass }]
+  ],
+  {
+    formatName: 'camelCase',
+    resolverOptions: {
+      lifetime: Lifetime.SINGLETON
+    }
+  }
+)
+
+container.registerServices = (app) => {
+  app.use(scopePerRequest(container))
+}
+
+module.exports = container
