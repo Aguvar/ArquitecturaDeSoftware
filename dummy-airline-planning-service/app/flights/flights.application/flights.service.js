a0eee5cecd3f8550ac39330c012fc431194f2388
Augusto Varela <gustivarela@hotmail.com>
2019-06-21 19:28:17 -0300

    Implement Airline Planning Dummy Server
    Correct .env.example details

---
+++ dummy-airline-planning-service/app/flights/flights.application/flights.service.js
@@ -0,0 +1,17 @@
+const moment = require('moment')
+
+class FlightsService {
+  async validate (flight) {
+    let receivedDate = Date.now()
+    flight.receivedDate = receivedDate
+    // Log time between flight sent and arrived
+    let momentReceived = moment.unix(flight.receivedDate)
+    let momentSent = moment.unix(flight.SentDate)
+
+    let latency = momentReceived.diff(momentSent)
+
+    console.log(`Latency: ${latency} ms`)
+  }
+}
+
+module.exports = FlightsService
