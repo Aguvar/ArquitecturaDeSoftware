badee2966cba823c7cc9f349375648f967475f5d
Augusto Varela <gustivarela@hotmail.com>
2019-06-21 19:50:26 -0300

    Update dates to timestamps

--- dummy-airline-maintenance-service/app/flights/flights.application/flights.service.js
+++ dummy-airline-maintenance-service/app/flights/flights.application/flights.service.js
@@ -2,11 +2,11 @@ const moment = require('moment')
 
 class FlightsService {
   async validate (flight) {
-    let receivedDate = Date.now()
-    flight.receivedDate = receivedDate
+    let receivedTimestamp = Date.now()
+    flight.receivedTimestamp = receivedTimestamp
     // Log time between flight sent and arrived
-    let momentReceived = moment.unix(flight.receivedDate)
-    let momentSent = moment.unix(flight.sentDate)
+    let momentReceived = moment.unix(flight.receivedTimestamp)
+    let momentSent = moment.unix(flight.sentTimestamp)
 
     let latency = momentReceived.diff(momentSent)
 
--- dummy-airline-planning-service/app/flights/flights.application/flights.service.js
+++ dummy-airline-planning-service/app/flights/flights.application/flights.service.js
@@ -2,11 +2,11 @@ const moment = require('moment')
 
 class FlightsService {
   async validate (flight) {
-    let receivedDate = Date.now()
-    flight.receivedDate = receivedDate
+    let receivedTimestamp = Date.now()
+    flight.receivedTimestamp = receivedTimestamp
     // Log time between flight sent and arrived
-    let momentReceived = moment.unix(flight.receivedDate)
-    let momentSent = moment.unix(flight.SentDate)
+    let momentReceived = moment.unix(flight.receivedTimestamp)
+    let momentSent = moment.unix(flight.SentTimestamp)
 
     let latency = momentReceived.diff(momentSent)
 
